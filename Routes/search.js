70881f954c194dea4b37422b8fd6eae944718fed
najamalamkhan1 <najamalamkhan1321@gmail.com>
2026-05-15 15:35:48 +0500

    minor bugs fixes

--- Routes/search.js
+++ Routes/search.js
@@ -1109,7 +1109,7 @@ router.get(
       // =========================
 
       const featuredBrands =
-        await FeaturedBrand.find({
+        await featuredBrands.find({
           active: true
         }).lean();
 
